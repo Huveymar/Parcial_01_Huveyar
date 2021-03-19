c1f0f630c866d5ca481fb56526acf2a7d1441e88
Huveymar Muñoz <wilsonmunoz2020@itp.edu.co>
2021-03-18 21:01:50 -0500

    avance parcial

--- src/models/productos.py
+++ src/models/productos.py
@@ -5,13 +5,15 @@ class ProductosModel():
     def traerTodos(self):
         cursor = globals.DB.cursor()
 
-        cursor.execute('select * from productos ')
+        cursor.execute('show databases ')
 
         productos = cursor.fetchall()
 
         cursor.close()
 
         return productos
+
+
 
     def crear(self, nombre,descripcion,precio_compra, precio_venta,estado):
         cursor = globals.DB.cursor()
